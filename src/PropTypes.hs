310a59e015f02df0578757dd4327c2393f9779a6
Milo <mikkelmilo@gmail.com>
2019-02-12 19:03:25 +0100

    added a bunch of theorems about propositional logic

--- src/PropTypes.hs
+++ src/PropTypes.hs
@@ -32,10 +32,6 @@ module PropTypes where
 
     data Or a b = OrL a | OrR b
 
-    -- ex falso quodlibet
-    absurd :: Void -> a
-    absurd contra = case contra of {}
-
     andI :: a -> b -> And a b
     andI a b = (a,b)
 
@@ -55,5 +51,66 @@ module PropTypes where
     orE (OrL a) f _ = f a
     orE (OrR b) _ g = g b
      
+
+    -- Various (fairly trivial) theorems/tautologies of propositional logic.
+    -- Inspired by the theorems from https://www.cs.princeton.edu/courses/archive/fall07/cos595/stdlib/html/Coq.Init.Logic.html
+
+    -- ex falso quodlibet
+    absurd :: Void -> a
+    absurd contra = case contra of {}
+
+    not_False :: Not Void
+    not_False = \contra -> case contra of {}
+
+    double_neg :: a -> Not (Not a) -- same as a -> (a -> Void) -> Void 
+    double_neg x contra = contra x -- same as \x -> \contra -> contra x
+
+    contrapositive :: (a -> b) -> (Not b -> Not a)
+    contrapositive ab = \notb -> notb . ab -- pretty neat
+
+    not_both_true_false :: Not (And a (Not a))
+    not_both_true_false (a, not_a) = not_a a
+
+    contra_implies_anything :: And a (Not a) -> b
+    contra_implies_anything (a, not_a) = absurd (not_a a)
+
     impliesTrans :: (a -> b) -> (b -> c) -> a -> c
     impliesTrans f g = g . f -- equivalent to impliesTrans f g x = g (f x)
+
+    iff_refl :: Iff a a
+    iff_refl = andI (\x -> x) (\x -> x)
+
+    iff_trans :: (Iff a b) -> (Iff b c) -> (Iff a c)
+    iff_trans (ab, ba) (bc, cb) = andI (bc . ab) (ba . cb) -- equivalent to andI (\x -> bc (ab x)) (\x -> ba (cb x))
+
+    iff_sym :: (Iff a b) -> (Iff b a)
+    iff_sym (ab, ba) = andI ba ab
+
+    and_comm :: (And a b) -> (And b a)
+    and_comm (a, b) = andI b a
+
+    and_assoc :: (And a (And b c)) -> (And (And a b) c)
+    and_assoc (a, (b, c)) = andI (andI a b) c 
+
+    and_symm :: Iff (And a b) (And b a) 
+    and_symm = andI and_comm and_comm -- pretty neat and concise, right?
+
+    or_comm :: (Or a b) -> (Or b a)
+    or_comm or_ab = case or_ab of
+        OrL a -> orIR a
+        OrR b -> orIL b
+    
+    or_assoc :: Iff (Or a (Or b c)) (Or (Or a b) c)
+    or_assoc = andI -- maybe this can be made shorter?
+        (\or_a_bc -> case or_a_bc of
+            OrL a -> orIL (orIL a)
+            OrR or_bc -> case or_bc of
+                OrL b -> orIL (orIR b)
+                OrR c -> orIR c    
+        )
+        (\or_ab_c -> case or_ab_c of
+            OrL or_ab -> case or_ab of 
+                OrL a -> orIL a
+                OrR b -> orIR (orIL b)
+            OrR c -> orIR (orIR c)
+        )
